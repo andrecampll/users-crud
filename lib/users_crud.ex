ca76698a5bdaee5b9cd2a8d55207900f42b738f0
André <andrevictor50@gmail.com>
2020-11-08 17:55:07 -0300

    defining create user operation

--- lib/users_crud.ex
+++ lib/users_crud.ex
@@ -15,4 +15,14 @@ defmodule UsersCrud do
   def hello do
     :world
   end
+
+  alias UsersCrud.Repo, as: DB
+
+  import Ecto.Query
+
+  def insert(first_name, last_name, email, age) do
+    user = %UsersCrud.User{first_name: first_name, last_name: last_name, email: email, age: age}
+
+    user |> DB.insert()
+  end
 end
